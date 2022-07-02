a055eed56525fcf321a06921a6a55b347eb20574
Jesther Costinar <jesther.jc15@gmail.com>
2022-07-02 21:39:11 +0800

    Set up database and Routes

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,6 +1,37 @@
-<?php 
+<?php
 
-class Controller {
-    
+class Controller
+{
+    // load model    
+    public function model($model)
+    {
+        $model = ucwords($model);
+        if (file_exists('../app/models/' . $model . '.Model.php')) {
+            require_once('../app/models/' . $model . '.Model.php');
+            return new $model();
+        } else
+            throw new Exception("Model: '" . $model . "' does not Exists");
+    }
+
+    // load view
+    public function view($view, $data = [])
+    {
+        $view = str_replace(".", "/", $view);
+        if (file_exists('../app/views/' . $view . '.php')) {
+            require_once('../app/views/' . $view . '.php');
+        } else
+            throw new Exception("View: '" . $view . "' does not Exists");
+    }
+
+    // load the 404 page
+    public function errorRedirect()
+    {
+        $this->view("front.404");
+    }
+
+    // load the 404 page in admin section
+    public function adminErrorRedirect()
+    {
+        $this->view("admin.404");
+    }
 }
-?>
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,6 +1,72 @@
-<?php 
+<?php
 
-class Core {
-    
+class Core
+{
+    protected $currentController = 'Home';
+    protected $currentMethod = 'index';
+    protected $params = [];
+
+    public function __construct()
+    {
+        $url = $this->getUrl();
+        $url = $this->getController($url);
+        $url = $this->getMethod($url);
+        // Get parameters
+        $this->params = $url ? array_values($url) : [];
+
+        // Call a callback with array of params
+        if (isset($this->currentMethod)) {
+            try {
+                call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
+            } catch (Exception $e) {
+                echo $e;
+            }
+        }
+    }
+
+    // get Url
+    public function getUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url']);
+            $url = trim($url, '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            return explode('/', $url);
+        }
+    }
+
+    // get Controller    
+    public function getController($url)
+    {
+        try {
+            if (isset($url[0])) {
+                if (file_exists("../app/controllers/" . ucwords($url[0]) . '.Controller.php')) {
+                    $this->currentController = ucwords($url[0]);
+                    unset($url[0]);
+                }
+            }
+
+            require "../app/controllers/" . $this->currentController . ".Controller.php";
+            $this->currentController = new $this->currentController;
+            return $url;
+        } catch (Exception $e) {
+            echo "</br> <span style='color:red'>No Page Found </span>";
+        }
+    }
+
+    // get Method
+    public function getMethod($url)
+    {
+        try {
+            if (isset($url[1])) {
+                if (method_exists($this->currentController, $url[1])) {
+                    $this->currentMethod = $url[1];
+                    unset($url[1]);
+                }
+                return $url;
+            }
+        } catch (Exception $e) {
+            echo "</br> <span style='color:red'>No method Found </span>";
+        }
+    }
 }
-?>
